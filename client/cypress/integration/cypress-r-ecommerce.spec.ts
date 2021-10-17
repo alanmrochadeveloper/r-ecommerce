a986d8e638de3cd8a55256ebaca03afa890b7ba4
alan rocha sotran tmov <alan.rocha@sotran.com.br>
2021-10-17 18:14:51 -0300

    test: tweaked cypress variables and test on apis

--- client/cypress/integration/cypress-r-ecommerce.spec.ts
+++ client/cypress/integration/cypress-r-ecommerce.spec.ts
@@ -1,13 +1,27 @@
+const basePath = '/'
+const api=Cypress.env('api');
+
 describe('Acceptance test', () => {
 	it('Should visit the site and print ROCHA on the screen', () => {
-		cy.visit(Cypress.env('baseUrl'));
+		cy.visit(basePath);
 		cy.get('.App').contains('ROCHA');
 	});
 
+	
+
 	it('Should visit website and encounters 3 buttons ["Início", "Contato", "Sobre"]', ()=>{
-		cy.visit(Cypress.env('baseUrl'));
+		cy.visit(basePath);
 		cy.get('button*[class*="inicio"]').contains('Início')
 		cy.get('button*[class*="contato"]').contains('Contato')
 		cy.get('button*[class*="sobre"]').contains('Sobre')
+	})
+
+	it('Should test api answer with code 200 ok', ()=>{
+		cy.request(api+'/init-test/test').its('status').should('eq', 200)
+	})
+
+	it('Should show in the inital page the api answer rendered in the document screen',()=>{
+		cy.visit(basePath)
+		cy.get('.api-container').contains("It is working")
 	})
 });
