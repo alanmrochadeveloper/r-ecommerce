c15f8d998fcf54fada5fdc3c14f949221e49b4f3
alan rocha sotran tmov <alan.rocha@sotran.com.br>
2021-11-28 10:52:03 -0300

    chore: added database mapping

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -8,16 +8,23 @@ import { OrderModule } from './order/order.module';
 import { OrderlineModule } from './orderline/orderline.module';
 import { PersonalInfoModule } from './personal-info/personal-info.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PersonalInfo } from './personal-info/entities/personal-info.entity';
+import { Orderline } from './orderline/entities/orderline.entity';
+import { Order } from './order/entities/order.entity';
+import { Brand } from './brand/entities/brand.entity';
+import { Category } from './category/entities/category.entity';
+import { Product } from './product/entities/product.entity';
+import { User } from './user/entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
+    type: 'postgres',
     host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: 'test',
-    entities: [],
+    port: 15432,
+    username: 'postgres',
+    password: 'postgres',
+    database: '#Postgres123',
+    entities: [User, Product, Category, Brand, Order, Orderline, PersonalInfo],
     synchronize: true,
   }), InitTestModule, UserModule, ProductModule, CategoryModule, BrandModule, OrderModule, OrderlineModule, PersonalInfoModule]
 })
--- api/src/orderline/orderline.module.ts
+++ api/src/orderline/orderline.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrderlineService } from './orderline.service';
 import { OrderlineController } from './orderline.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Orderline } from './entities/orderline.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Orderline])],
   controllers: [OrderlineController],
   providers: [OrderlineService]
 })
