dfe19682f2f67fcd395fb776ba19c347728a6405
alan rocha sotran tmov <alan.rocha@sotran.com.br>
2021-12-14 01:09:04 -0300

    chore: added more attr to product model

--- client/src/models/product.ts
+++ client/src/models/product.ts
@@ -1,9 +1,29 @@
+export enum InventoryType  {
+  IN_STOCK = 'in stock',
+  LIMITED = 'limited',
+  OUT_OF_STOCK = 'out of stock',
+} 
+
 export interface IProduct {
     id: string;
+    attributes?: string;
     title: string;
     description: string;
     brand: string;
     category: string;
-    image: string;
+    image?: string;
     price: string;
+    createdAt: number | string;
+    currency?: string;
+    inventoryType?: InventoryType;
+    isAvailable?: boolean;
+    isShippable?: boolean;
+    quantity: number;
+    updatedAt: number | string;
+    images: string [];
+    includesTaxes: boolean;
+    isTaxable: boolean;
+    productCode: string;
+    productSku: string;
+    salePrice: string;
 }
